48aa3b8d7e93a460a672032e0456482175d784c7
huancasfrank0-hue <huancasfrank0@gmail.com>
2026-01-08 16:00:02 -0500

    Fix: Interfaz embebida en el binario y limpieza de dependencias

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     // Configuración para que se vea bien en pantallas HighDPI
     QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-    
+
     QGuiApplication app(argc, argv);
     app.setWindowIcon(QIcon::fromTheme("multimedia-audio-player"));
     app.setOrganizationName("NebulaOS");
@@ -20,16 +20,15 @@ int main(int argc, char *argv[])
     // Inyectamos el C++ al QML como "Backend"
     engine.rootContext()->setContextProperty("Backend", &player);
 
-    // Lógica inteligente para encontrar el archivo Main.qml
-    // Busca en la misma carpeta donde está el ejecutable
-    const QUrl url = QUrl::fromLocalFile(app.applicationDirPath() + "/Main.qml");
+    // NUEVA LÓGICA: Ahora carga desde el sistema de recursos interno (qrc)
+    const QUrl url(QStringLiteral("qrc:/Main.qml"));
 
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
-    
+
     engine.load(url);
 
     return app.exec();
